f46ae6e31fcff14db67ea6f624b35564eb8a56f6
lzxl30 <2775864744@qq.com>
2026-07-11 22:48:31 +0800

    Create categories.js

---
+++ categories.js
@@ -0,0 +1,16 @@
+// 所有分类的元信息（按显示顺序排列）
+var categoryConfigs = [
+  {
+    folder: 'turing',
+    id: 'turing',
+    name: '图灵魔法',
+    icon: ''
+  }
+  // 未来添加新分类，只需在这里追加，例如：
+  // {
+  //   folder: 'images',
+  //   id: 'images',
+  //   name: '图片处理',
+  //   icon: '🖼️'
+  // }
+];
