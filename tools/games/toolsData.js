fbe1aecbe0e1898306058ceebb0966118b7f6809
lzxl30 <2775864744@qq.com>
2026-07-12 14:12:09 +0800

    Update toolsData.js

--- tools/games/toolsData.js
+++ tools/games/toolsData.js
@@ -1,1 +1,12 @@
-
+var gamesTools = [
+  {
+    id: 'games-1',
+    title: '表情合成',
+    url: 'https://emojikitchen.net/zh-CN/',
+    description: 'Emoji 表情融合工具，选择两个表情即可生成创意合成表情，趣味十足。',
+    category: 'games',
+    icon: '😀',
+    iconBgClass: 'bg-3',
+    accentClass: 'accent-3'
+  }
+];
