8b3573105a682831e43fa2808c912529c68c6edd
lzxl30 <2775864744@qq.com>
2026-07-21 14:12:22 +0800

    Update toolsData.js

--- tools/tools/toolsData.js
+++ tools/tools/toolsData.js
@@ -1,1 +1,13 @@
-
+var toolsTools = [
+  {
+    id: 'tools-1',
+    title: 'Yandex 搜索',
+    url: 'https://yandex.com/search/',
+    description: '俄罗斯最大的搜索引擎，支持全球网页、图片、视频搜索。',
+    category: 'tools',
+    icon: '🔍',
+    iconBgClass: 'bg-4',      // 蓝紫渐变，理性工具感
+    accentClass: 'accent-4',
+    
+  }
+];
