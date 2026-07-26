fc8152ba5d7a3ed51813e31f96bd1d1bd933056f
lzxl30 <2775864744@qq.com>
2026-07-26 21:20:23 +0800

    Update toolsData.js

--- tools/movie/toolsData.js
+++ tools/movie/toolsData.js
@@ -1,1 +1,12 @@
-
+var sceneryTools = [
+  {
+  id: 'movie-1',
+  title: '以图搜番',
+  url: 'https://trace.moe/',
+  description: '上传动漫截图，精准搜索出处、集数及时间点，找番神器。',
+  category: 'movie',
+  icon: '🔍',
+  iconBgClass: 'bg-4',          // 蓝紫渐变，科技感与观影氛围
+  accentClass: 'accent-4',
+  }
+];
