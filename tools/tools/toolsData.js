24cb9dc6a31364e2bde8c53bec63d6bdb942b9c8
lzxl30 <2775864744@qq.com>
2026-07-28 21:22:37 +0800

    Update toolsData.js

--- tools/tools/toolsData.js
+++ tools/tools/toolsData.js
@@ -19,5 +19,15 @@ var toolsTools = [
   icon: '🎙️',
   iconBgClass: 'bg-0',
   accentClass: 'accent-0',
-  }
+  },
+  {
+    id: 'games-5',
+    title: '便携小空调',
+    url: 'https://air.shadiao.pro/',
+    description: '在线模拟空调，调节温度与模式，界面逼真，适合夏天望梅止渴。',
+    category: 'games',
+    icon: '❄️',
+    iconBgClass: 'bg-4',
+    accentClass: 'accent-4'
+  },
 ];
